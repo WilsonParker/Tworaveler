db449cf2ace377eaa29a75c9499ec3e229d4a1a6
Tacademy <kimgim7311@gmail.com>
2017-08-25 13:43:54 +0900

    ui

--- app/src/main/java/com/developer/hare/tworaveler/Activity/BagDelete.java
+++ app/src/main/java/com/developer/hare/tworaveler/Activity/BagDelete.java
@@ -14,6 +14,7 @@ import com.developer.hare.tworaveler.Data.DataDefinition;
 import com.developer.hare.tworaveler.Data.SessionManager;
 import com.developer.hare.tworaveler.Model.BagModel;
 import com.developer.hare.tworaveler.Model.Response.ResponseArrayModel;
+import com.developer.hare.tworaveler.Model.Response.ResponseModel;
 import com.developer.hare.tworaveler.Model.UserModel;
 import com.developer.hare.tworaveler.Net.Net;
 import com.developer.hare.tworaveler.R;
@@ -32,7 +33,6 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
-import static android.R.string.no;
 import static com.developer.hare.tworaveler.Data.DataDefinition.Bag.CATEGORY_MAP;
 import static com.developer.hare.tworaveler.Data.DataDefinition.Bag.CATEGORY_SALE;
 import static com.developer.hare.tworaveler.Data.DataDefinition.Bag.CATEGORY_SHOP;
@@ -198,9 +198,9 @@ public class BagDelete extends AppCompatActivity {
                 }
             });
         }else{
-            Net.getInstance().getFactoryIm().oneDeleteBagItemList().enqueue(new Callback<ResponseArrayModel<String>>() {
+            Net.getInstance().getFactoryIm().oneDeleteBagItemList(Integer.parseInt(nos+"")).enqueue(new Callback<ResponseModel<String>>() {
                 @Override
-                public void onResponse(Call<ResponseArrayModel<String>> call, Response<ResponseArrayModel<String>> response) {
+                public void onResponse(Call<ResponseModel<String>> call, Response<ResponseModel<String>> response) {
                     if (response.isSuccessful()) {
                         // 성공했을 경우
                         switch (response.body().getSuccess()) {
@@ -228,7 +228,7 @@ public class BagDelete extends AppCompatActivity {
                 }
 
                 @Override
-                public void onFailure(Call<ResponseArrayModel<String>> call, Throwable t) {
+                public void onFailure(Call<ResponseModel<String>> call, Throwable t) {
                     netFailAlert(R.string.bagDelete_alert_title_fail, R.string.bagDelete_alert_content_fail_2);
                 }
             });
